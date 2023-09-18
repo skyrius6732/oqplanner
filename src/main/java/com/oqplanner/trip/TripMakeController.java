0e996593002e1d0e997e1011a0c806e6c369a011
skyrius <skyrius6732@gmail.com>
2023-09-18 11:27:39 +0900

    swagger config 설정 및 api swagger 정보 업데이트

--- src/main/java/com/oqplanner/trip/TripMakeController.java
+++ src/main/java/com/oqplanner/trip/TripMakeController.java
@@ -4,9 +4,17 @@ package com.oqplanner.trip;
 import com.oqplanner.tripplan.service.TripPlanService;
 import com.oqplanner.tripuser.domain.TripUser;
 import com.oqplanner.tripuser.service.TripUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name="trip-make-controller",  description = "여행 생성 Controller 입니다.")
 @RestController
 @RequestMapping("/trip/user")
 public class TripMakeController {
@@ -20,6 +28,11 @@ public class TripMakeController {
         this.tripPlanService = tripPlanService;
     }
 
+    @Operation(summary = "여행자 저장", description = "여행자 저장 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "사용자 정보 저장 성공", content = @Content(schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @PostMapping("/info")
     public int saveUserInfo(@RequestBody TripUser tripUser) {
         int num = tripUserService.saveUserInfo(tripUser);
--- src/main/java/com/oqplanner/tripplan/controller/TripPlanController.java
+++ src/main/java/com/oqplanner/tripplan/controller/TripPlanController.java
@@ -4,37 +4,67 @@ package com.oqplanner.tripplan.controller;
 import com.oqplanner.tripplan.domain.TripPlan;
 import com.oqplanner.tripplan.service.TripPlanService;
 import com.oqplanner.tripuser.service.TripUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@Tag(name="trip-plan-controller",  description = "여행 계획 Controller 입니다.")
 @RestController
 @RequestMapping("/trip/plan")
 public class TripPlanController {
 
     private final TripPlanService tripPlanService;
 
+
     @Autowired
     public TripPlanController(TripUserService tripUserService, TripPlanService tripPlanService) {
         this.tripPlanService = tripPlanService;
     }
 
+    @Operation(summary = "여행계획 저장", description = "여행계획 저장 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "여행계획 정보 저장 성공", content = @Content(schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @PostMapping("/info")
     public int savePlanInfo(@RequestBody TripPlan tripPlan) {
         return tripPlanService.savePlanInfo(tripPlan);
     }
 
+    @Operation(summary = "여행계획 조회", description = "여행계획 조회 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "여행계획 조회 성공", content = @Content(schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @GetMapping("/info")
     public List<TripPlan> getPlanInfo(@RequestBody TripPlan tripPlan) {
         return tripPlanService.getPlanInfoList(tripPlan);
     }
 
+    @Operation(summary = "여행계획 삭제", description = "여행계획 삭제 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "여행계획 삭제 성공", content = @Content(schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @DeleteMapping("/info")
     public int removePlanInfo(@RequestBody TripPlan tripPlan){
         return tripPlanService.removePlanInfo(tripPlan);
     }
+
+
+    @Operation(summary = "여행계획 삭제", description = "여행계획 삭제 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "여행계획 삭제 성공", content = @Content(schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @DeleteMapping("/info/{tripPlanNo}")
     public int removePlanInfoBytripPlanNo(@PathVariable String tripPlanNo){
         return tripPlanService.removePlanInfoBytripPlanNo(tripPlanNo);
